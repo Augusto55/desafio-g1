7cafd0c76e477f50df3e8c315519e227d942c2ab
Augusto55 <85200864+Augusto55@users.noreply.github.com>
2023-09-24 02:52:41 -0300

    Zombie and Player Behaviour

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,18 +10,102 @@ public class NewBehaviourScript : MonoBehaviour
     float playerX;
     float playerY;
 
+    float health;
+    float speed;
+
     Rigidbody2D rb;
+    Animator anim;
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform shootingPosition;
+    public float bulletSpeed = 25;
+    Vector2 lookDirection;
+    float lookAngle;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+
+
+        health = 3;
     }
 
     void Update()
+    {
+        Move();
+        Shooting();
+        Flip();
+
+        anim.SetInteger("speed", (int)playerX);
+
+    }
+
+    void Move()
     {
         playerX = Input.GetAxis("Horizontal");
         playerY = Input.GetAxis("Vertical");
 
         rb.velocity = new Vector2(playerX * xSpeed, playerY * ySpeed);
+
+        speed = Mathf.Abs(playerX); // Use Mathf.Abs para obter um valor positivo
+        Debug.Log(playerX);
     }
+
+    void Shooting()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        lookDirection = mousePosition - (Vector2)shootingPosition.position;
+        lookDirection.Normalize();
+
+        lookAngle = Mathf.Atan2(lookDirection.x, lookDirection.y) * Mathf.Rad2Deg;
+
+        shootingPosition.rotation = Quaternion.Euler(0, 0, lookAngle);
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bulletClone = Instantiate(bullet);
+            bulletClone.transform.position = shootingPosition.position;
+
+            float lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+
+            Debug.Log(lookDirection);
+
+            bulletClone.transform.rotation = Quaternion.Euler(0, 0, lookAngle);
+
+            bulletClone.GetComponent<Rigidbody2D>().velocity = lookDirection * bulletSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+            {
+            Debug.Log(health);
+            health -= 1;
+
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void Flip()
+    {
+        if (lookDirection.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        else if (lookDirection.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+
+
 }
