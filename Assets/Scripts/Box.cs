3610055e5a6def552a8707557d338cf2148737c6
Augusto55 <85200864+Augusto55@users.noreply.github.com>
2023-09-26 19:41:16 -0300

    Update Box.cs

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class open_box : MonoBehaviour
+public class Box : MonoBehaviour
 {
     bool haveGun = false;
     bool boxClean = true;
